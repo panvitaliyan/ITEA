5f2c07e9938e5ff56c728fff428ebc840018acb5
VitaliyPanchenko <panvitaliyan@gmail.com>
2021-10-19 20:47:51 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        File currentFile = new File("./");        
+        //while (true){
+            
+            
+            for (File iFile : currentFile.listFiles()) {
+                if(iFile.isDirectory())
+                    System.out.println(iFile.getName());
+            }
+            for (File iFile : currentFile.listFiles()) {
+                if(iFile.isFile())
+                    System.out.println(iFile.getName());
+            }
+            
+        //}
+        scanner.close();
+    }
+    
+}
